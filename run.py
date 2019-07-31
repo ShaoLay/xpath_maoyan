74d29164fb5c9c0a436fa617d1f63681ef2638ab
Hamdi <javs_shao@163.com>
2019-07-31 19:26:42 +0800

    Update run.py
    清洗数据_增加评分数据

--- run.py
+++ run.py
@@ -22,9 +22,10 @@ while page < 91:
     movie_urls = html.xpath('//p[@class="name"]/a/@href')
     img_urls = html.xpath('//img[@class="board-img"]/@src')
     stars = html.xpath('//p[@class="star"]//text()')
+    score_ints = html.xpath('//i[@class="integer"]//text()')
+    score_floats = html.xpath('//i[@class="fraction"]//text()')
 
-    for title, movie_url, star in zip(titles, movie_urls, stars):
+    for title, movie_url, star, score_int, score_float in zip(titles, movie_urls, stars, score_ints, score_floats):
         movies_urls = 'https://maoyan.com' + movie_url
-        print u'电影:' + title + u'\t电影地址:' + str(movies_urls) + '\t' + star
-
-
+        scores = str(score_int) + str(score_float)
+        print u'电影:' + title + u'\t电影地址:' + str(movies_urls) + '\t' + star + u'\t评分:' + scores
